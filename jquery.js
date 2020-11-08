f368ac3a05849733500e36119ba4d7b096e0f3e3
Saker07 <58092882+Saker07@users.noreply.github.com>
2020-11-08 15:41:30 +0100

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    $('#header-inner').toggleClass('scrolling', $(window).scrollTop() > $('#header').offset().top);
+    $('#header-inner').toggleClass('banner', $(window).scrollTop() > $('#header').offset().top);
   });
 });
